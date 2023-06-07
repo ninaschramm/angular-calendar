237895e0d4f79a0972a06d24b22621d4cf3aa244
ninaschramm <schrammnina@outlook.com>
2023-06-07 12:09:47 -0300

    delete and change date features

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,5 +1,24 @@
 import { Component } from '@angular/core';
 
+interface Appointment {
+  title: string;
+  date: Date;
+  time: string;
+}
+
+// Helper function to format timezone offset
+function getTimezoneOffsetString(offset: number): string {
+  const sign = offset > 0 ? "-" : "+";
+  const hours = Math.floor(Math.abs(offset) / 60);
+  const minutes = Math.abs(offset) % 60;
+  return `${sign}${padNumber(hours)}:${padNumber(minutes)}`;
+}
+
+// Helper function to pad number with leading zero
+function padNumber(num: number): string {
+  return String(num).padStart(2, "0");
+}
+
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
@@ -11,6 +30,14 @@ export class CalendarComponent {
   currentYear: number;
   weeks: any[] = [];
   currentMonthName: string = '';
+  weekdays: string[] = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']; 
+  appointments: Appointment[] = [];
+  appointmentTitle: string = '';
+  appointmentDate: string = '';
+  appointmentTime: string = '';
+  selectedDate: Date = new Date();
+
+
 
   constructor() {
     this.currentDate = new Date();
@@ -97,6 +124,66 @@ export class CalendarComponent {
     const date = new Date();
     date.setMonth(month);
     return date.toLocaleString('en-US', options);
+  }
+  
+  addAppointment() {
+    if (this.appointmentTitle && this.appointmentDate && this.appointmentTime) {
+      const timezoneOffset = new Date().getTimezoneOffset();
+      const newAppointment: Appointment = {
+    title: this.appointmentTitle,
+    date: new Date(`${this.appointmentDate}T00:00:00${getTimezoneOffsetString(timezoneOffset)}`),
+    time: this.appointmentTime
+  };
+      this.appointments.push(newAppointment);
+      this.clearAppointmentForm();
+    }
+  }
+  
+
+  clearAppointmentForm() {
+    this.appointmentTitle = '';
+    this.appointmentDate = '';
+    this.appointmentTime = '';
+  }
+
+  getAppointmentsForDate(date: Date): Appointment[] {
+    return this.appointments.filter(appointment => {
+      return (
+        appointment.date.getFullYear() === date.getFullYear() &&
+        appointment.date.getMonth() === date.getMonth() &&
+        appointment.date.getDate() === date.getDate()
+      );
+    });
+  }
+
+  hasAppointmentsForDay(day: number): boolean {
+    if (day === null) {
+      return false;
+    }
+    const date = new Date(this.currentYear, this.currentMonth, day);
+    return this.getAppointmentsForDate(date).length > 0;
+  }
+
+  selectDay(day: number) {
+    if (day !== null) {
+      this.selectedDate = new Date(this.currentYear, this.currentMonth, day);
+    }
+  }  
+  
+  deleteAppointment(appointment: Appointment) {
+    const index = this.appointments.indexOf(appointment);
+    if (index !== -1) {
+      this.appointments.splice(index, 1);
+    }
+  }
+
+  changeAppointmentDate(appointment: Appointment) {
+    const newDate = prompt('Enter a new date (YYYY-MM-DD):');
+    const timezoneOffset = new Date().getTimezoneOffset();
+    if (newDate) {
+      appointment.date = new Date(`${newDate}T00:00:00${getTimezoneOffsetString(timezoneOffset)}`);      
+    }
+    console.log(this.appointments)
   }
   
   
